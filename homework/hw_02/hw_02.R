9adfe3355e4dc628a858a5a85635d398303c514d
Trystan Bordeau <tbordeau@vols.utk.edu>
2020-08-30 14:13:00 -0400

    initial commit

---
+++ homework/hw_02/hw_02.R
@@ -0,0 +1,14 @@
+#####
+### This is a simple script that will run 
+### a mathematical equation
+#######
+
+# create the variables you want to use
+a <- 2
+b <- 2
+
+# assign the variables to another variable 
+d <- a + b 
+
+# this will be the answer 
+d
